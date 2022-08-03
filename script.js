e34ca64c85ae76cb16463916a3057ccbf741e840
bmilcs <bmilcs@yahoo.com>
2022-08-03 18:48:31 -0400

    Style container and add colors

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const body = document.getElementsByTagName("body")[0];
+const section = document.createElement("section");
 const container = document.createElement("div");
+const header = document.createElement("h1");
 const rows = [];
 const squares = [];
 const clrNeutral900 = "#000000";
 const clrNeutral100 = "#ffffff";
-let sizeOfGrid = 100;
+let sizeOfGrid = 50;
 
+// spacebar: clears screen of painted div's
 document.addEventListener("keyup", (e) => {
   if (e.keyCode === 32) {
     resetGrid();
@@ -26,22 +29,31 @@ for (let row = 0; row < sizeOfGrid; row++) {
     squares[squareNumber] = document.createElement("div");
     squares[squareNumber].classList.add("square");
 
-    // squares[squareNumber].classList.add("colorMe")
-    squares[squareNumber].addEventListener(
-      "mouseover",
-      () => (squares[squareNumber].style.backgroundColor = "black")
+    squares[squareNumber].addEventListener("mouseover", () =>
+      squares[squareNumber].classList.add("color-me")
     );
+    // () => (squares[squareNumber].style.backgroundColor = "black")
+    // () => squares[squareNumber].classList.add("colorMe"));
 
     rows[row].appendChild(squares[squareNumber]);
   }
   container.appendChild(rows[row]);
 }
 
+container.classList.add("container");
+
+// setup header
+// header.textContent = "Etch-A-Sketch";
+// section.appendChild(header);
+
+// add container to section
+section.appendChild(container);
+
+body.appendChild(section);
+
+// reset bg color / wipe drawing
 function resetGrid() {
   squares.forEach((square) => {
-    square.style.backgroundColor = "white";
+    square.classList.remove("color-me");
   });
 }
-
-container.classList.add("container");
-body.appendChild(container);
