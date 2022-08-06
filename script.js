c57b908fd54e871097eff43bb9d03a2a1d525a17
bmilcs <bmilcs@yahoo.com>
2022-08-06 14:50:18 -0400

    Add createGrid/removeGrid functions and clearBtn eventlistener

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const body = document.getElementsByTagName("body")[0];
 const etchASketch = document.getElementById("etch-a-sketch");
 const container = document.createElement("div");
+const slider = document.getElementById("grid-size-range");
+const clearBtn = document.getElementById("clear-btn");
+const rainbowBtn = document.getElementById("rainbow-btn");
 const rows = [];
 const squares = [];
 const clrNeutral900 = "#000000";
 const clrNeutral100 = "#ffffff";
+let sizeOfGrid = 16;
 let drawingToggle;
-let sizeOfGrid = 64;
 
 console.log(etchASketch);
 
-// spacebar: clears screen of painted div's
-document.addEventListener("keyup", (e) => {
-  if (e.keyCode === 32) {
-    resetGrid();
-  }
-});
+createGrid(sizeOfGrid);
+
+// setup main etch-a-sketch container
+container.classList.add("container");
+container.setAttribute("draggable", false);
+
+// add container > section element > body element
+etchASketch.appendChild(container);
+
+// slider onchange: set sizeOfGrid to slider.value
+slider.onchange = function () {
+  removeGrid();
+  createGrid(this.value);
+  console.log("grid resizing: " + this.value);
+};
+
+// clear button: reset grid
+clearBtn.onclick = () => resetGrid();
 
 // mousedown: enable drawing
 document.addEventListener("mousedown", (e) => {
@@ -35,33 +50,34 @@ function toggleDrawing(x) {
   else container.removeEventListener("mouseover", colorMe);
 }
 
-// setup main etch-a-sketch container
-container.classList.add("container");
-container.setAttribute("draggable", false);
+// create etch-a-sketch grid
+function createGrid(newSize) {
+  for (let row = 0; row < newSize; row++) {
+    console.log(rows[row]);
+    rows[row] = document.createElement("div");
+    rows[row].classList.add("row");
+    rows[row].setAttribute("draggable", false);
 
-// create sizeOfGrid number of rows:
-for (let row = 0; row < sizeOfGrid; row++) {
-  rows[row] = document.createElement("div");
-  rows[row].classList.add("row");
-  rows[row].setAttribute("draggable", false);
-
-  // within each row, create sizeOfGrid number of squares:
-  for (let square = 0; square < sizeOfGrid; square++) {
-    // prevent each iteration from overwriting the previous 1-16
-    let sq = sizeOfGrid * row + square;
-    squares[sq] = document.createElement("div");
-    squares[sq].classList.add("square");
-    squares[sq].setAttribute("draggable", false);
-    rows[row].appendChild(squares[sq]);
+    // within each row, create sizeOfGrid number of squares:
+    for (let square = 0; square < newSize; square++) {
+      // prevent each iteration from overwriting the previous 1-16
+      let sq = newSize * row + square;
+      squares[sq] = document.createElement("div");
+      squares[sq].classList.add("square");
+      squares[sq].setAttribute("draggable", false);
+      rows[row].appendChild(squares[sq]);
+    }
+    // append row of 16 squares to final container
+    container.appendChild(rows[row]);
   }
-  // append row of 16 squares to final container
-  container.appendChild(rows[row]);
 }
 
-// add container > section element > body element
-etchASketch.appendChild(container);
-
-// functions
+// remove all rows from the container
+function removeGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
 
 // mouseover callback: highlights square via css properties
 let colorMe = function (square) {
